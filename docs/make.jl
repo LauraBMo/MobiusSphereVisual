89820c7a3b200a785af251d97336fc525cfe81c6
LauBMo <15877199+LauraBMo@users.noreply.github.com>
2025-10-04 15:41:54 +0200

    Expand documentation for rendering presets

--- docs/make.jl
+++ docs/make.jl
@@ -3,21 +3,25 @@ using Documenter
 
 DocMeta.setdocmeta!(MobiusSphereVisual, :DocTestSetup, :(using MobiusSphereVisual); recursive=true)
 
-makedocs(;
-    modules=[MobiusSphereVisual],
-    authors="LauBMo <laurea987@gmail.com> and contributors",
-    sitename="MobiusSphereVisual.jl",
-    format=Documenter.HTML(;
-        canonical="https://LauraBMo.github.io/MobiusSphereVisual.jl",
-        edit_link="main",
-        assets=String[],
+makedocs(
+    modules = [MobiusSphereVisual],
+    authors = "LauBMo <laurea987@gmail.com> and contributors",
+    sitename = "MobiusSphereVisual.jl",
+    format = Documenter.HTML(
+        canonical = "https://LauraBMo.github.io/MobiusSphereVisual.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
+        "Rendering guide" => Any[
+            "Quality presets" => "quality_presets.md",
+            "Sampling overrides" => "sampling_overrides.md",
+        ],
     ],
 )
 
-deploydocs(;
-    repo="github.com/LauraBMo/MobiusSphereVisual.jl",
-    devbranch="main",
+deploydocs(
+    repo = "github.com/LauraBMo/MobiusSphereVisual.jl",
+    devbranch = "main",
 )
